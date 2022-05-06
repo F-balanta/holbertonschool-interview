c5be6c3921a607f2d0599ac0203eede65367822b
F-balanta <jf.balanta@hotmail.com>
2022-05-05 21:21:41 -0500

    update task

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,54 +1,61 @@
 #include "binary_trees.h"
-
-heap_t *find_parent_node(heap_t **root, int value);
-
 /**
- * heap_insert - Inserts a value into a Max Binary Heap
- * @root: A double pointer to the root node of the Heap
- * @value: The value to store in the node to be inserted
- *
- * Return: A pointer to the inserted node or NULL if failed
+ * heap_insert - Insert value into a Binary Heap
+ * @root: Double pointer to the root node of the Heap
+ * @value: The value store in the node to be inserted
+ * Return: A tree with max heap ordering
  */
+
 heap_t *heap_insert(heap_t **root, int value)
 {
-	binary_tree_t *new;
-	binary_tree_t *parent;
-
-	parent = find_parent_node(root, value);
-
-	new = binary_tree_node(parent, value);
-
-	if (parent == NULL)
-	{
-		*root = new;
-		return (new);
-	}
-	return (NULL);
-}
-
-/**
- * find_parent_node - Returns the parent node of node to insert
- * @root: A double pointer to the root node of the Heap
- * @value: The value to compare to find parent node
- *
- * Return: A pointer to the parent node or NULL if root
- */
-heap_t *find_parent_node(heap_t **root, int value)
-{
-	binary_tree_t *temp = *root;
-	binary_tree_t *left = NULL;
-	binary_tree_t *right = NULL;
+	heap_t *node, *parent;
 
 	if (*root == NULL)
-		return (NULL);
-
-	if (temp->n < value)
-		return (temp);
-
-	while (temp)
 	{
-		return (left);
+		node = binary_tree_node(*root, value);
+		*root = node;
+		return (*root);
 	}
+	node = *root;
+	while (node != NULL)
+	{
+		parent = node;
+		if (node->n == value)
+			return node_swap(node);
+		if (value > node->n)
+			node = parent->left;
+		else
+			node = node->right;
+	}
+	if (value > parent->n)
+	{
+		parent->left = binary_tree_node(parent, value);
+		return (parent->left);
+	}
+	else
+	{
+		parent->right = binary_tree_node(parent, value);
+		return (parent->right);
+	}
+}
 
-	return (right);
+
+/**
+ * node_swap - Swaps current node with larger node
+ * @node: Pointer to current node
+ * Return: A new node
+ */
+heap_t *node_swap(heap_t *node)
+{
+	while (node && node->parent)
+	{
+		while (node->n > node->parent->n)
+		{
+			node = node->parent;
+			node->n += node->parent->n;
+			node->parent->n -= node->n;
+			node->parent->n = node->n - node->parent->n;
+		}
+	}
+	return (node);
 }
