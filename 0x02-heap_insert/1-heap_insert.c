27898d6fdbeb6480d491de4792a45cc31c1c21e3
F-balanta <jf.balanta@hotmail.com>
2022-05-05 21:59:29 -0500

    update task

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,24 +1,54 @@
-#include"binary_trees.h"
+#include "binary_trees.h"
+
+heap_t *find_parent_node(heap_t **root, int value);
+
 /**
- * heap_insert - function that inserts a value into a Binary Heap
- * @root: is a double pointer to the root node of the Heap
- * @value: is the value store in the node to be inserted
+ * heap_insert - Inserts a value into a Max Binary Heap
+ * @root: A double pointer to the root node of the Heap
+ * @value: The value to store in the node to be inserted
  *
- * Return: a pointer to the inserted node, or NULL on failure
- **/
+ * Return: A pointer to the inserted node or NULL if failed
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node = NULL;
+	binary_tree_t *new;
+	binary_tree_t *parent;
 
-	new_node = malloc(sizeof(*new_node));
+	parent = find_parent_node(root, value);
 
-	if (!new_node)
+	new = binary_tree_node(parent, value);
+
+	if (parent == NULL)
+	{
+		*root = new;
+		return (new);
+	}
+	return (NULL);
+}
+
+/**
+ * find_parent_node - Returns the parent node of node to insert
+ * @root: A double pointer to the root node of the Heap
+ * @value: The value to compare to find parent node
+ *
+ * Return: A pointer to the parent node or NULL if root
+ */
+heap_t *find_parent_node(heap_t **root, int value)
+{
+	binary_tree_t *temp = *root;
+	binary_tree_t *left = NULL;
+	binary_tree_t *right = NULL;
+
+	if (*root == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->parent = *root;
+	if (temp->n < value)
+		return (temp);
 
-	return (new_node);
+	while (temp)
+	{
+		return (left);
+	}
+
+	return (right);
 }
