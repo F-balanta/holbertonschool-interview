85f5fc084938b3d696e8409d8e32b781a46bb7b0
F-balanta <jf.balanta@hotmail.com>
2022-05-26 20:37:37 -0500

    update task

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,108 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * check_recursively - Recursively moves the pointers to check if palindrome
+ * @start: Will move from the start of the list
+ * @end: Will move from the end of the list
+ *
+ * Return: Either 0 or 1 depending if is a palindrome
  */
-size_t print_listint(const listint_t *h)
+int recursive(listint_t **start, listint_t *end)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+	int check;
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
+	if (end == NULL)
+		return (1);
 
-    return (n);
-}
+	check = recursive(start, end->next);
 
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-    listint_t *new;
-    listint_t *current;
+	if (check == 0)
+		return (0);
 
-    current = *head;
+	if ((*start)->n == end->n)
+		check = 1;
+	else
+		check = 0;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	*start = (*start)->next;
 
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
-
-    return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+	return (check);
 }
 /**
- * is_palindrome - checks if a singly linked list is a palindrome.
+ * is_palindrome - Checks if a singly linked list is a palindrome
  * 
- * @head: Fist parameter 
- * Return: int 
+ * @head: The start of the linked list
+ * Return: int
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *ini = *head;
-	int i = 0, j = 0;
-	int l[4096];
+	listint_t **start = head;
+	listint_t *end = *head;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (1);
-	while (ini->next)
-		ini = ini->next, i++;
-	ini = *head;
-	while (ini)
-	{
-		l[j] = ini->n;
-		ini = ini->next;
-		j++;
-	}
-	ini = *head;
-	while (ini)
-	{
-		if (ini->n == l[i])
-			ini = ini->next;
-		else
-			return (0);
-		i--;
-	}
-	return (1);
+
+	return (recursive(start, end));
 }
