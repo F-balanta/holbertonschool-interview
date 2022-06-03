b690d8e5776f764426a9ea1c13bf4a7e5009e6e9
F-balanta <jf.balanta@hotmail.com>
2022-06-02 19:54:43 -0500

    Project log parsing update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,43 +1,47 @@
 #!/usr/bin/python3
-"""standard input"""
-import sys
-import fileinput
+"""
+0x06-log_parsing
+"""
+from sys import stdin
 
-i = 0
-filesize = 0
 
-status = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
+def printstats(file_size, status_codes):
+    """
+    Print statistics method
+    """
+    print("File size: " + str(file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(code + ": " + str(status_codes[code]))
+
+
+line_num = 0
+file_size = 0
+status_code = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
-    for line in fileinput.input():
-        i += 1
-        delim = line.split()
-        code = delim[len(delim) - 2]
-        if code in status.keys():
-            status[code] += 1
-        size = delim[len(delim) - 1]
-        if size.isdigit():
-            filesize += int(size)
-        if i % 10 == 0:
-            print(f"File size: {filesize}")
-            for item in sorted(status.items()):
-                if item[1] != 0:
-                    print(f"{item[0]}: {item[1]}")
-    print(f"File size: {filesize}")
-    for item in sorted(status.items()):
-        if item[1] != 0:
-            print(f"{item[0]}: {item[1]}")
-except KeyboardInterrupt:
-    print("File size: {}".format(filesize))
-    for item in sorted(status.items()):
-        if item[1] != 0:
-            print("{}: {}".format(item[0], item[1]))
+    for line in stdin:
+        line_num += 1
+        split_line = line.split()
+
+        if len(split_line) > 1:
+            file_size += int(split_line[-1])
+
+        if len(split_line) > 2 and split_line[-2].isnumeric():
+            status_code = split_line[-2]
+        else:
+            status_code = 0
+
+        if status_code in status_codes.keys():
+            status_codes[status_code] += 1
+
+        if line_num % 10 == 0:
+            printstats(file_size, status_codes)
+
+    printstats(file_size, status_codes)
+
+except (KeyboardInterrupt):
+    printstats(file_size, status_codes)
+    raise
