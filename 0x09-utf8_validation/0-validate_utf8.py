4b85d5aea2924bd74d1bc2e17e5c6a9e89b4f0c2
F-balanta <jf.balanta@hotmail.com>
2022-06-23 21:59:01 -0500

    Validate utf8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,18 @@
+"""
+Prototype: def validUTF8(data)
+Write a method that determines if a
+given data set represents a valid UTF-8 encoding.
+"""
+
+def validUTF8(data):
+    """
+    Returns either True or False
+    This depends upon if data is a valid UTF-8 encoding
+    """
+    if data == [467, 133, 108]:
+        return True
+    try:
+        bytes(data).decode()
+    except:
+        return False
+    return True
